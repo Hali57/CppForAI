9a9522e46c1b1bae84afc84814f2d0bde3bd38fe
Hali57 <halifaxhali77@gmail.com>
2025-02-28 12:48:49 +0300

    "A  c++ program that asks a user for their name and age and outputs a greeting message"

--- CppForAI/CppForAI.cpp
+++ CppForAI/CppForAI.cpp
@@ -1,6 +1,21 @@
 ﻿#include<iostream>
-
+#include<string>
 
 int main() {
-	std::cout << "Hello, AI World! 🚀" << std::endl;
+
+	// a c++ program that asks a user for their name and age and outputs a greeting message
+
+	std::string name{};
+	int age{};
+
+	std::cout << "What is your name?" << std::endl;
+
+	std::cin >> name;
+
+	std::cout << "How old are you ?" << std::endl;
+
+	std::cin >> age;
+
+	std::cout << "Hello " << name << " nice to meet you and you are  " << age << " years old" << std::endl;
+
 }
